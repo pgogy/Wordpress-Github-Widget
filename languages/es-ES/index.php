b8ba2a58bb4aafa93b4ba97a6fb046b9ee548545
Juan Antonio Ruiz Rivas <juananruizrivas@gmail.com>
2020-04-12 18:05:31 +0200

    Spanish translation

---
+++ languages/es-ES/index.php
@@ -0,0 +1,16 @@
+<?php
+
+	$github_show_string = "Muestra mis repositorios";
+	$github_hide_string = "Oculta mis repositorios";
+	$github_percent_string = "porcentaje de commits";
+	$github_error = "Error GitHub API: ";
+	$github_description = "Un widget para mostrar perfiles de github";
+	$github_name = 'Perfil de Github';
+	$github_username = 'Usuario GitHub';
+	$github_password = 'Contraseña GitHub';
+	$github_warning = 'Si la contraseña no es correcta puede producir un error por exceso de tiempo en la API';
+	$github_repo = 'La lista de repositorios está oculta por defecto';
+	$github_no = "No";
+	$github_yes = "Si";
+	
+?>
